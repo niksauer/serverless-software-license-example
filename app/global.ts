24b443ed6e8100f947d4a153c27b9d10427355f7
Nik Sauer <nik.sauer@me.com>
2020-06-13 12:13:47 +0200

    Abstract global setup and move to SASS

---
+++ app/global.ts
@@ -0,0 +1,2 @@
+import './assets/icons';
+import './assets/styles/index.global.scss';
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,9 @@
 import React, { Fragment } from 'react';
+
 import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
-import './app.global.css';
+
+import './global';
 import App from './App';
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
