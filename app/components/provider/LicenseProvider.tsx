47eaa743e252bc9e77f58353bb7641aac297ad1f
Nik Sauer <nik.sauer@me.com>
2020-06-15 23:55:51 +0200

    Continue demo setup of SSL lib

--- app/components/provider/LicenseProvider.tsx
+++ app/components/provider/LicenseProvider.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-// import { LicenseManager, LicenseRegistry } from 'serverless-software-license';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  LicenseManager,
+  LicenseRegistry,
+  ILicenseStorage,
+  LicenseManagerEvent
+} from 'serverless-software-license';
+import { ethers } from 'ethers';
 
 interface LicenseState {
   isValid: boolean;
@@ -10,20 +16,36 @@ const LicenseContext = React.createContext<LicenseState>({
 });
 
 export const LicenseProvider: React.FC = ({ children }) => {
-  // const [isValid, setIsValid] = useState(false);
-  // const registry = useMemo(() => new LicenseRegistry(''), []);
-  // const manager = useMemo(() => new LicenseManager(), []);
+  const [isValid, setIsValid] = useState(false);
 
-  // useEffect(() => {
-  //   manager.emitter.on(
-  //     LicenseManagerEvent.LicenseValidityChanged,
-  //     (isLicenseValid: boolean) => {
-  //       setIsValid(isLicenseValid);
-  //     }
-  //   );
+  const provider = useMemo(
+    () => new ethers.providers.JsonRpcProvider('http://localhost:8545'),
+    []
+  );
+  const registry = useMemo(
+    () =>
+      new LicenseRegistry(
+        '0x0585cCdc5dFA71264C509ef687Ae4E814BA9Cd7E',
+        provider
+      ),
+    [provider]
+  );
+  const storage = useMemo(() => ({} as ILicenseStorage), []);
+  const manager = useMemo(() => new LicenseManager(registry, storage), [
+    registry,
+    storage
+  ]);
 
-  //   manager.checkValidity();
-  // });
+  useEffect(() => {
+    manager.emitter.on(
+      LicenseManagerEvent.LicenseValidityChanged,
+      (isLicenseValid: boolean) => {
+        setIsValid(isLicenseValid);
+      }
+    );
+
+    // manager.checkValidity();
+  });
 
   return (
     <LicenseContext.Provider value={{ isValid: false }}>
