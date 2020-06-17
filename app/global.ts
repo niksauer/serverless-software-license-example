06d188ceee9cc5d09e012ad06b45c08e09807e46
Nik Sauer <nik.sauer@me.com>
2020-06-17 19:46:09 +0200

    Move to hash history to enable app wide reload

--- app/global.ts
+++ app/global.ts
@@ -1,9 +1,11 @@
 import './assets/icons';
 import './assets/styles/index.global.scss';
 import './environment';
-import { createBrowserHistory, History, LocationState } from 'history';
+import { History, LocationState, createHashHistory } from 'history';
 
 // https://github.com/ReactTraining/history/blob/v4/docs/GettingStarted.md
 // https://github.com/ReactTraining/react-router/blob/master/FAQ.md#how-do-i-access-the-history-object-outside-of-components
 // eslint-disable-next-line import/prefer-default-export
-export const history: History<LocationState> = createBrowserHistory();
+export const history: History<LocationState> = createHashHistory({
+  basename: window.location.href
+});
